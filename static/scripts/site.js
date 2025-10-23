e28db1ec3aee0c623efc72e94f21672708374819
MBerklc <klcmberk@gmail.com>
2025-10-23 10:19:45 +0100

    week5v1

---
+++ static/scripts/site.js
@@ -0,0 +1,7 @@
+function chgContent() {
+  document.getElementById("ccon").innerHTML = "Paragraph changed.";
+}
+function changeColor(color) {
+    document.getElementById("ccon").style.color = color;
+}
+<!Add alert>
